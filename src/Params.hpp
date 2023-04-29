df7f75d1465e4ec9cc3d7b86f36658e0b57ab6fc
MaroonSkull <nikobelikkg@gmail.com>
2023-04-29 21:53:39 +0300

    change class to struct

--- src/Params.hpp
+++ src/Params.hpp
@@ -2,12 +2,12 @@
 
 #include <vector>
 
-class Params1D {
-	std::vector<float> k{}; // значения наклонов на углах отрезков
+struct Params1D {
 	// numsteps_ = k.size
 	float step{};
 	uint32_t dx{};
 	uint32_t controlPoints{};
 	uint32_t octaveNum{};
 	uint32_t resultDotsCols{};
+	std::vector<float> k{}; // value of slopes at the corners of segments
 };

8b738423dec87f8dff911b0d5b2d9ea797a53309
MaroonSkull <33577562+MaroonSkull@users.noreply.github.com>
2025-05-25 16:09:56 +0300

    add cpu realisation for 2d-noise

--- src/Perlin.hpp
+++ src/Perlin.hpp
@@ -1,18 +1,26 @@
-﻿#pragma once
+﻿#include <cstddef>
 #include <cstdint>
+#include <iostream>
+#include <memory>
+
+#ifdef __CUDACC__
+// #include <cuda/api/detail/unique_span.hpp>
+#include <cuda/api/error.hpp>
+#include <cuda/api/kernel_launch.hpp>
+#include <cuda/api/memory.hpp>
+#include <cuda/api.hpp>
+
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/host_vector.h>
+#include <thrust/random.h>
+#endif
+
 #include <vector>
-
-class Params1D {
-  std::vector<float> k{}; // значения наклонов на углах отрезков
-  // numsteps_ = k.size
-  float step{};
-  uint32_t dx{};
-  uint32_t controlPoints{};
-  uint32_t octaveNum{};
-  uint32_t resultDotsCols{};
-};
-
-// ****************** Device functions ******************
+#include <random>
+#include <algorithm>
+#ifdef __CUDACC__
+namespace kernels {
 
 /**
  * @brief Линейная интерполяция.
@@ -70,42 +78,52 @@ template <typename T> __device__ __forceinline__ T smootherstep_kernel(T x) {
 
 /**
  * Вычисление одномерного шума Перлина.
- * Вычисляет массив любой длины, допустимой видеокартой (для CC3.0+ это (2^31 −
- * 1)*2^10 ≈ 2.1990233e+12 значений)
+ * Вычисляет массив любой длины, допустимой видеокартой
+ * (для CC3.0+ это (2^31 − 1)*2^10 ≈ 2.1990233e+12 значений)
  *
- * \param noise – массив с результатом вычисления шума перлина на оси.
- * \param octave – массив для хранения первой октавы шума Перлина.
- * \param k – массив со значениями наклона уравнений в контрольных узлах.
- * \param size – длина массива noise.
- * \param step – величина шага между точками, в которых вычисляется шум.
- * \param numSteps – количество точек между контрольными узлами.
+ * \par[ret] noise – массив с результатом вычисления шума перлина на оси.
+ * \par[ret] octave – массив для хранения первой октавы шума Перлина.
+ * \param gradients – массив со значениями наклона уравнений в контрольных
+ * узлах.
+ * \param axisLength – длина массива noise.
+ * \param axisStep – величина шага между точками, в которых вычисляется шум.
+ * \param pointsBetweenGradients – количество точек между контрольными узлами.
  * \param isOctaveCalkNeed – будут ли в дальнейшем вычисляться октавы.
- *
- * \return noise – (см. описание параметра) функция изменяет переданный массив
- * (хранится в памяти GPU).
- * \return octave – (см. описание параметра) функция изменяет переданный массив
- * (хранится в памяти GPU).
  */
 template <typename T>
-__global__ void Perlin1D_kernel(T *noise, T *octave, const T *k, uint32_t size,
-                                T step, uint32_t numSteps,
+__global__ void Perlin1D_kernel(T *noise, T *octave, const T *gradients,
+                                uint32_t axisLength, T axisStep,
+                                uint32_t pointsBetweenGradients,
                                 bool isOctaveCalkNeed) {
-  uint32_t id =
-      blockIdx.x * blockDim.x + threadIdx.x; // [0..] – всего точек для просчёта
-  if (id >= size)
+  // количество threads, выполняющих вычисления
+  uint32_t id = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (id >= axisLength)
     return;
-  uint32_t n = id * step; // 0 0 0 / 1 1 1 / 2 2 2 / .. – какие точки к каким
-                          // контрольным точкам принадлежат
-  uint32_t dotNum = id % numSteps; // 0 1 2 / 0 1 2 / 0 1 2 / .. – позиция точки
-                                   // между левым и правым значением
-  T t = dotNum * step; // 0.0 0.33 0.66 / 0 0.33 0.66 / .. – численное значение
-                       // точки для интерполяции
-  t = smoothstep_kernel<T>(t); // Применяем сигмоидальную(на промежутке [0, 1])
-                               // функцию, реализуя градиент
-  T y0 = k[n] * t;             // kx+b (b = 0)
-  T y1 = k[n + 1] * (t - 1);   // kx+b (b = -k) = k(x-1)
-  noise[id] = lerp_kernel<T>(
-      y0, y1, t); // Интерполяцией находим шум, пишем сразу в выходной массив
+
+  // 0 0 0 / 1 1 1 / 2 2 2 / .. – какие точки шума к каким контрольным узлам
+  // принадлежат
+  uint32_t n = id * axisStep;
+
+  // 0 1 2 / 0 1 2 / 0 1 2 / .. – позиция точки между левым и правым контрольным
+  // узлом
+  uint32_t dotNum = id % (pointsBetweenGradients + 1);
+
+  // 0.0 0.33 0.66 / 0 0.33 0.66 / .. – численное значение точки для
+  // интерполяции
+  T t = dotNum * axisStep;
+
+  // Применяем сигмоидальную(на промежутке [0, 1]) функцию, реализуя градиент
+  t = smoothstep_kernel<T>(t);
+
+  // kx+b (b = 0)
+  T y0 = gradients[n] * t;
+
+  // kx+b (b = -k) = k(x-1)
+  T y1 = gradients[n + 1] * (t - 1);
+
+  // Интерполяцией находим шум, пишем сразу в выходной массив
+  noise[id] = lerp_kernel<T>(y0, y1, t);
 
   // Если нужно вычислять октавы, сохраняем в памяти первую окатву шума
   if (isOctaveCalkNeed)
@@ -300,85 +318,387 @@ __global__ void Perlin1Dvertices_kernel(T *vertices, const T *noise,
   }
 }
 
+} // namespace kernels
+#endif
+
 /**
- * Вспомогательная функция для вычисления шума Перлина на оси.
- *
- * @param noise Массив с результатом вычисления шума перлина на оси.
- * @param k Массив со значениями наклона уравнений в контрольных узлах.
- * @param step Величина шага между точками, в которых вычисляется шум.
- * @param numSteps Количество точек между контрольными узлами.
- * @param controlPoints Количество узлов.
- * @param resultDotsCols Количество точек для просчёта.
- * @param octaveNum Количество накладывающихся октав на шум.
- *
- * @return noise Функция изменяет переданный массив.
- * @return cudaError_t
+ * @brief Интерфейс библиотеки
  */
-template <typename T> void Perlin1D(T *vertices, const Params1D &p) {
-  T *dev_vertices = nullptr; // pointer to noise array in VRAM
-  T *dev_noise = nullptr;    // pointer to noise array in VRAM
-  T *dev_octave = nullptr;   // pointer to temp array in VRAM
-  T *dev_k =
-      nullptr; // pointer to array with tilt angle (tg slope angle) in VRAM
+template <class T = float, class Container_t = std::vector<T>>
+class IPerlin {
+public:
+  /// Размерность пространства (1D, 2D, 3D...)
+  std::size_t numberOfDimensions_{1};
 
-  // Choose which GPU to run on.
-  cudaSetDevice(0);
+  /// шаг между узлами градиентов [1, +inf)
+  std::size_t pointsBetweenGradients_{1};
 
-  // Allocate GPU buffers for arrays.
-  cudaMalloc((void **)&dev_vertices, 2 * (p.resultDotsCols + 1) * sizeof(T));
+  /// количество векторов градиентов
+  std::size_t numberOfGradients_{1};
 
-  // последняя точка (+1) замыкает шум, она всегда = 0
-  cudaMalloc((void **)&dev_noise, (p.resultDotsCols + 1) * sizeof(T));
+  /// the number of levels of detail you want you perlin noise to have.
+  std::size_t numberOfOctaves_{0};
 
-  // Массив для октав займёт в 2 раза меньше памяти.
-  if (p.octaveNum > 0)
-    // округление вниз при целочисленном делении - не ошибка
-    cudaMalloc((void **)&dev_octave, p.resultDotsCols * sizeof(T) / 2);
+  /// number that determines how much detail is added or removed at each octave (adjusts frequency)
+  std::size_t lacunarity_{2};
 
-  cudaMalloc((void **)&dev_k, p.controlPoints * sizeof(T));
+  /// number that determines how much each octave contributes to the overall shape (adjusts amplitude).
+  std::size_t persistence_{2};
 
-  // Copy input vectors from host memory to GPU buffers.
-  cudaMemcpy(dev_k, p.k_.data(), p.controlPoints * sizeof(T),
-             cudaMemcpyHostToDevice);
-
-  dim3 threadsPerBlock(p.resultDotsCols, 1, 1);
-  dim3 blocksPerGrid(1, 1, 1);
-
-  // 256 взято с потолка из каких-то общих соображений, забейте.
-  if (p.resultDotsCols > 256) {
-    threadsPerBlock.x = 256;
-    blocksPerGrid.x = (p.resultDotsCols + 255) / 256;
+  /**
+   * @brief Длина одной оси
+   * @details Количество опорных точек + количество промежутков между ними,
+   * умноженное на длину одного промежутка
+   */
+  std::size_t getAxisLenght() {
+    auto numberOfIntervals{numberOfGradients_ - 1};
+    auto numberOfNodes{numberOfGradients_};
+    return pointsBetweenGradients_ * numberOfIntervals + numberOfNodes;
   }
 
-  // Launch a kernel on the GPU with one thread for each element.
-  Perlin1D_kernel<T><<<blocksPerGrid, threadsPerBlock>>>(
-      dev_noise, dev_octave, dev_k, p.resultDotsCols, p.step, p.numSteps,
-      static_cast<bool>(p.octaveNum));
-
-  std::function<void(void)> sharedKernel;
-  // Выполняем наложение октав на получившийся шум.
-  if (p.octaveNum) {
-    if (p.resultDotsCols <=
-        2 * 32 * 1024 / sizeof(T)) // если вся октава помещается в разделяемую
-                                   // память, вызываем простое ядро
-      Perlin1Doctave_shared_kernel<T><<<blocksPerGrid, threadsPerBlock>>>(
-          dev_noise, dev_octave, p.resultDotsCols, p.octaveNum);
-    else // если октава не помещается целиком, вызываем сложное ядро.
-      Perlin1Doctave_shared_unlimited_kernel<T>
-          <<<blocksPerGrid, threadsPerBlock>>>(dev_noise, dev_octave,
-                                               p.resultDotsCols, p.octaveNum);
+  T getDistanceBetweenTwoPoints() {
+    return 1.0 / (pointsBetweenGradients_ + 1);
   }
 
-  Perlin1Dvertices_kernel<T><<<blocksPerGrid, threadsPerBlock>>>(
-      dev_vertices, dev_noise, p.resultDotsCols);
+  /**
+   * @brief Конструктор класса Perlin с параметрами размерности и шага
+   * градиента.
+   *
+   * @param N Размерность пространства шума.
+   * @param gradientStep Шаг градиента для вычисления шума.
+   */
+  IPerlin(
+    std::size_t numberOfDimensions,
+    std::size_t pointsBetweenGradients,
+    std::size_t numberOfGradients,
+    std::size_t numberOfOctaves,
+    std::size_t lacunarity,
+    std::size_t persistence
+  ) :
+    numberOfDimensions_{numberOfDimensions},
+    pointsBetweenGradients_{pointsBetweenGradients},
+    numberOfGradients_{numberOfGradients},
+    numberOfOctaves_{numberOfOctaves},
+    lacunarity_{lacunarity},
+    persistence_{persistence} {}
 
-  // Copy output vector from GPU buffer to host memory.
-  cudaMemcpy(vertices, dev_vertices, 2 * (p.resultDotsCols + 1) * sizeof(T),
-             cudaMemcpyDeviceToHost);
+  IPerlin() = default;
 
-  // clean-up
-  cudaFree(dev_vertices);
-  cudaFree(dev_noise);
-  cudaFree(dev_octave);
-  cudaFree(dev_k);
-}
+  virtual ~IPerlin() = default;
+
+  virtual Container_t getNoise() = 0;
+};
+
+#ifdef __CUDACC__
+/**
+ * @brief Реализация шума Перлина с использованием NVidia CUDA
+ */
+template <class T = float, class Container_t = thrust::host_vector<T>>
+class PerlinGPU_CUDA : public IPerlin<T, Container_t> {
+  
+  using i = IPerlin<T, Container_t>;
+
+  /// Линеаризованный массив градиентов
+  thrust::host_vector<T> h_gradients_;
+  thrust::device_vector<T> d_gradients_;
+
+  /// Значения шума во всех точках пространства
+  thrust::host_vector<T> h_noise_;
+  thrust::device_vector<T> d_noise_;
+  thrust::device_vector<T> d_octave_;
+
+
+  // Инициализация градиентов случайными значениями
+  void initializeGradients() {
+    std::cout << "Initializing gradients..." << std::endl;
+
+    h_gradients_.clear();
+    h_gradients_.shrink_to_fit();
+    h_gradients_.resize(i::numberOfDimensions_ * i::numberOfGradients_ + 1);
+
+    static thrust::default_random_engine rng(1337);
+    static thrust::uniform_real_distribution<T> dist(-1, 1);
+
+    thrust::generate(h_gradients_.begin(), h_gradients_.end(),
+                     [&] { return dist(rng); });
+            
+    // закольцовываем градиент
+    h_gradients_[i::numberOfDimensions_ * i::numberOfGradients_] = h_gradients_[0];
+    
+    d_gradients_ = h_gradients_;
+  }
+
+  void calculateNoise() {
+    std::cout << "Calculating noise..." << std::endl;
+
+    h_noise_.clear();
+
+    // Количество точек пространства - это длина одной оси в степени измерений.
+    std::size_t numberOfPoints = std::pow(i::getAxisLenght(), i::numberOfDimensions_);
+    std::size_t sizeOfPoint = i::numberOfDimensions_;
+
+    d_noise_.resize(sizeOfPoint * numberOfPoints);
+    d_octave_.resize(d_noise_.size() / 2);
+
+    if (i::numberOfDimensions_ == 1) {
+      // Получаем raw-указатели на device данные
+      T *d_noise = thrust::raw_pointer_cast(d_noise_.data());
+      T *d_octave = thrust::raw_pointer_cast(d_octave_.data());
+      const T *d_gradients = thrust::raw_pointer_cast(d_gradients_.data());
+
+      auto launch_config =
+          cuda::launch_config_builder()
+              .block_size(numberOfPoints > 256 ? 256 : numberOfPoints)
+              .grid_size(numberOfPoints > 256 ? (numberOfPoints + 255) / 256
+                                              : 1)
+              .build();
+
+      std::cout << "CUDA kernel launch with " << launch_config.dimensions.grid.x
+                << " blocks of " << launch_config.dimensions.block.x
+                << " threads each\n";
+
+      // Launch a kernel on the GPU with one thread for each element.
+      cuda::launch(kernels::Perlin1D_kernel<T>, launch_config, d_noise,
+                   d_octave, d_gradients, numberOfPoints, i::getDistanceBetweenTwoPoints(),
+                   i::pointsBetweenGradients_, i::numberOfOctaves_);
+
+      if (cuda::outstanding_error::get() != cuda::status_t::CUDA_SUCCESS) {
+        std::cerr << "CUDA error occurred during kernel execution\n";
+        return;
+      }
+
+      // Проверка ошибок ядра
+      cudaError_t kernelError = cudaGetLastError();
+      if (kernelError != cudaSuccess) {
+        std::cerr << "CUDA kernel error: " << cudaGetErrorString(kernelError)
+                  << std::endl;
+      }
+
+      // Синхронизация для корректного завершения
+      cudaDeviceSynchronize();
+    }
+
+    h_noise_ = d_noise_;
+  }
+
+public:
+
+  /**
+   * @brief Конструктор класса Perlin с параметрами размерности и шага
+   * градиента.
+   *
+   * @param N Размерность пространства шума.
+   * @param gradientStep Шаг градиента для вычисления шума.
+   */
+  PerlinGPU_CUDA(
+    std::size_t numberOfDimensions,
+    std::size_t pointsBetweenGradients,
+    std::size_t numberOfGradients,
+    std::size_t numberOfOctaves,
+    std::size_t lacunarity,
+    std::size_t persistence
+  ) : IPerlin<T, Container_t>(
+    numberOfDimensions,
+    pointsBetweenGradients,
+    numberOfGradients,
+    numberOfOctaves,
+    lacunarity,
+    persistence) {
+      initializeGradients();
+    }
+
+  PerlinGPU_CUDA() : IPerlin<T, Container_t>() {
+    initializeGradients();
+  }
+
+  thrust::host_vector<T> getNoise() {
+    calculateNoise();
+    return h_noise_;
+  }
+};
+#endif
+
+template<class T = float, class Container_t = std::vector<T>>
+class PerlinCPU : public IPerlin<T, Container_t> {
+  
+  using i = IPerlin<T, Container_t>;
+
+  /// Линеаризованный массив градиентов
+  Container_t gradients_;
+  
+  /// Значения шума во всех точках пространства
+  Container_t noise_;
+
+  // Инициализация градиентов случайными значениями
+  void initializeGradients() {
+    std::cout << "Initializing gradients..." << std::endl;
+
+    gradients_.clear();
+    gradients_.shrink_to_fit();
+    // + 1 точка в конце закольцовывает шум
+    gradients_.resize(i::numberOfDimensions_ * i::numberOfGradients_ + 1);
+
+    static std::default_random_engine rng(1337);
+    static std::uniform_real_distribution<T> dist(-1.0, 1.0);
+
+    std::generate(gradients_.begin(), --gradients_.end(),
+                     [&] { return dist(rng); });
+    
+    // закольцовываем градиент
+    gradients_.at(i::numberOfDimensions_ * i::numberOfGradients_) = gradients_.at(0);
+
+    noise_.resize(i::getAxisLenght());
+  }
+
+  /**
+   * @brief Линейная интерполяция.
+   *
+   * @details Функция вычисляет значение в точке t на промежутке [0, 1] между
+   * двумя прямыми с наклонами k0 и k1 соответственно.
+   *
+   * @param k0 Значение наклона прямой в точке 0.
+   * @param k1 Значение наклона прямой в точке 1.
+   * @param t Точка, значение в которой интерполируется.
+   *
+   * @return Результат интерполяции.
+   */
+  T lerp(T k0, T k1, T t) {
+    // (1-t)*k0 + t*k1 = k0 - t*k0 + t*k1 = t*(k1 - k0) + k0
+    return fma(t, k1 - k0, k0);
+  }
+
+  /**
+   * @brief Сигмоидальная функция из семейства smoothstep.
+   *
+   * @details Используется для создания более интенсивного градиента шума.
+   * Оригинальный первый полином.
+   *
+   * @param x Значение градиента (он же t).
+   *
+   * @return возвращает классический smoothstep(x).
+   *
+   * @see https://en.wikipedia.org/wiki/Smoothstep#Variations
+   */
+  T smoothstep(T x) {
+    // 3 * x^2 - 2 * x^3 = -x * x * (2 * x - 3);
+    return fma(static_cast<T>(2), x, static_cast<T>(-3)) * -x * x;
+  }
+
+  /**
+   * @brief Сигмоидальная функция из семейства smoothstep.
+   *
+   * @details Используется для создания ещё более интенсивного градиента шума.
+   * Оригинальный второй полином Кена Перлина.
+   *
+   * @see https://en.wikipedia.org/wiki/Smoothstep#Variations
+   *
+   * @param x Значение градиента (он же t).
+   *
+   * @return Возвращает классический smootherstep(x).
+   */
+  T smootherstep(T x) {
+    // 6x^5 - 15x^4 + 10x^3 = x^3(6x^2 - 15x + 10)
+    return fma(static_cast<T>(6), x * x,
+               fma(static_cast<T>(-15), x, static_cast<T>(10))) *
+           x * x * x;
+  }
+
+  /**
+   * @brief Вычисление одномерного шума Перлина.
+   */
+  void calculateBaseNoise() {
+    const auto noiseSize = i::getAxisLenght();
+    const auto axisStep = i::getDistanceBetweenTwoPoints();
+    for (int id = 0; id < noiseSize; id++) {
+      // 0 0 0 / 1 1 1 / 2 2 2 / .. – какие точки шума к каким контрольным узлам
+      // принадлежат
+      std::size_t n = id * axisStep;
+    
+      // 0 1 2 / 0 1 2 / 0 1 2 / .. – позиция точки между левым и правым контрольным
+      // узлом
+      uint32_t dotNum = id % (i::pointsBetweenGradients_ + 1);
+    
+      // 0.0 0.33 0.66 / 0 0.33 0.66 / .. – численное значение точки для
+      // интерполяции
+      T t = dotNum * axisStep;
+    
+      // Применяем сигмоидальную(на промежутке [0, 1]) функцию, реализуя градиент
+      t = smoothstep(t);
+    
+      // kx+b (b = 0)
+      T y0 = gradients_[n] * t;
+    
+      // kx+b (b = -k) = k(x-1)
+      T y1 = gradients_[n + 1] * (t - 1);
+    
+      // Интерполяцией находим шум, пишем сразу в выходной массив
+      noise_[id] = lerp(y0, y1, t);
+    }
+  }
+
+public:
+  PerlinCPU(
+    std::size_t numberOfDimensions,
+    std::size_t pointsBetweenGradients,
+    std::size_t numberOfGradients,
+    std::size_t numberOfOctaves,
+    std::size_t lacunarity,
+    std::size_t persistence
+  ) : IPerlin<T, std::vector<T>>(
+    numberOfDimensions,
+    pointsBetweenGradients,
+    numberOfGradients,
+    numberOfOctaves,
+    lacunarity,
+    persistence) {
+      initializeGradients();
+    }
+
+  PerlinCPU() : IPerlin<T, Container_t>() {
+    initializeGradients();
+  }
+
+  Container_t getNoise() {
+    calculateBaseNoise();
+    return noise_;
+  }
+};
+
+template <typename T>
+class PerlinFactory {
+public:
+#ifdef __CUDACC__
+  static std::unique_ptr<IPerlin<T, thrust::host_vector<T>>> createPerlin(
+#else
+  static std::unique_ptr<IPerlin<T, std::vector<T>>> createPerlin(
+#endif
+    std::size_t numberOfDimensions,
+    std::size_t pointsBetweenGradients,
+    std::size_t numberOfGradients,
+    std::size_t numberOfOctaves,
+    std::size_t lacunarity,
+    std::size_t persistence
+  ) {
+#ifdef __CUDACC__
+    std::cout << "Use CUDA" << std::endl;
+    return std::make_unique<PerlinGPU_CUDA<T>>(
+      numberOfDimensions, pointsBetweenGradients, numberOfGradients,
+      numberOfOctaves, lacunarity, persistence
+    );
+    #else
+    std::cout << "Use CPU" << std::endl;
+    return std::make_unique<PerlinCPU<T>>(
+      numberOfDimensions, pointsBetweenGradients, numberOfGradients,
+      numberOfOctaves, lacunarity, persistence
+    );
+#endif
+  }
+
+  static std::unique_ptr<IPerlin<T, std::vector<T>>> createPerlin() {
+#ifdef __CUDACC__
+    return std::make_unique<PerlinGPU_CUDA<T>>();
+#else
+    return std::make_unique<PerlinCPU<T>>();
+#endif
+  }
+};
